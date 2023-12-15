6d394700bbef18d31c5782f91d976c1a489515c3
nariman tleshev <230807@astanait.edu.kz>
2023-12-15 09:34:29 +0600

    rtdrtd

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt() , m = in.nextInt();
+        String[][] a = new String[n][m];
+        for(int i = 0;i < n;i++){
+            for(int j = 0;j < m;j++){
+
+            }
+        }
+
+
+    }
+}
